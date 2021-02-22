6a3f8a514cba9d2ceeff64fbbdb875f8e35c9651
Pablo Cianes <cianespablo@gmail.com>
2021-02-22 10:59:32 +0100

    ♻⚙ temporal change dev api url endpoint

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,12 @@
 const dev = {
-  endpoint: 'https://yf2qg97vc7.execute-api.eu-west-1.amazonaws.com/dev/payment',
+  //endpoint: 'https://yf2qg97vc7.execute-api.eu-west-1.amazonaws.com/dev/payment',
+  endpoint: 'https://dxbefy9wv1.execute-api.eu-west-1.amazonaws.com/dev/payment',
   STRIPE_KEY: 'pk_test_dWFGbfBf1OkmSoNS8QAu1R7a00HSKb10om',
 };
 
 const prod = {
-  endpoint: 'https://yf2qg97vc7.execute-api.eu-west-1.amazonaws.com/prod/payment',
+  //endpoint: 'https://yf2qg97vc7.execute-api.eu-west-1.amazonaws.com/prod/payment',
+  endpoint: 'https://dxbefy9wv1.execute-api.eu-west-1.amazonaws.com/prod/payment',
   STRIPE_KEY: 'pk_live_QzLlwFKdgLUAS3IzSOI1x9O800kdiQ1yrJ',
 };
 
