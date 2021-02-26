247eda0021d49bb5b12334b3f0b723bffc2b3710
Pablo Cianes <cianespablo@gmail.com>
2021-02-26 15:25:04 +0100

    ♻ refactor main content component

--- src/containers/Content.tsx
+++ src/containers/Content.tsx
@@ -1,46 +1,32 @@
 import React from 'react';
+import CtaBlock from '../components/CtaBlock';
+import Video from '../components/Video';
 import IconList from '../components/IconList';
 import ImageMosaic from '../components/ImageMosaic';
-import CtaButton from '../components/CtaButton';
 import { mainContent } from '../content/main';
-import Video from '../components/Video';
 
 const { button, mainCta, secondaryCta } = mainContent;
 const mainVideo = '/videos/video.mp4';
 const mainImage = '/videos/frame.jpg';
 
 const Content: React.FC<{ scrollTo: () => void }> = ({ scrollTo }) => {
+  const sectionClass = 'max-w-7xl mx-auto p-4 sm:p-6 lg:p-8 lg:text-center';
+
   return (
     <>
-      <section className="max-w-7xl mx-auto p-4 sm:p-6 lg:p-8 lg:text-center">
+      <section className={sectionClass}>
         <header>
-          <h1 className="text-base text-indigo-600 font-semibold tracking-wide uppercase">
-            {mainCta.title}
-          </h1>
-          <p className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
-            {mainCta.subtitle}
-          </p>
-          <h2 className="my-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">{mainCta.text}</h2>
-          <CtaButton text={button} onClick={scrollTo} />
+          <CtaBlock isMain={true} data={{ ...mainCta, button, fn: scrollTo }} />
         </header>
         <div className="my-10 md:my-20 mx-2 cursor-pointer">
           <Video image={mainImage} video={mainVideo} />
         </div>
       </section>
-      <section className="max-w-7xl mx-auto p-4 sm:p-6 lg:p-8 lg:text-center">
+      <section className={sectionClass}>
         <div className="lg:grid lg:grid-flow-row-dense lg:grid-cols-2 lg:gap-8 lg:items-center">
           <div className="lg:col-start-2 lg:max-w-2xl ml-auto">
             <header>
-              <h2 className="text-base leading-6 text-indigo-500 font-semibold uppercase">
-                {secondaryCta.title}
-              </h2>
-              <p className="mt-2 text-2xl leading-8 font-extrabold text-gray-900 dark:text-white sm:text-3xl sm:leading-9">
-                {secondaryCta.subtitle}
-              </p>
-              <h3 className="my-4 text-lg leading-6 text-gray-500 dark:text-gray-300">
-                {secondaryCta.text}
-              </h3>
-              <CtaButton text={button} onClick={scrollTo} />
+              <CtaBlock data={{ ...secondaryCta, button, fn: scrollTo }} />
             </header>
             <IconList />
           </div>
